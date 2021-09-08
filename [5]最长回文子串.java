0fbf9945f389a02b7d1bd96d8db26007c364d4c6
snmlm <913813112@qq.com>
2021-09-08 11:54:15 +0800

    update

--- [5]最长回文子串.java
+++ [5]最长回文子串.java
@@ -45,7 +45,7 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public String longestPalindrome(String s) {
-        
+
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
